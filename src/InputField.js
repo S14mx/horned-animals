19768778a821ffef4f10dc9e75e6337f4fd14ae3
S14mx <sergey.otryshko@gmail.com>
2021-12-22 16:43:19 -0800

    dynamic generation of options in the form

--- src/InputField.js
+++ src/InputField.js
@@ -3,17 +3,30 @@ import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 
 class InputField extends React.Component {
+  getUniqueOptions = () => {
+    const uniqueHornValues = [];
+    this.props.objectData.forEach(({ horns }) => {
+      if (!uniqueHornValues.includes(horns)) {
+        uniqueHornValues.push(horns);
+      }
+    });
+
+    return (
+      <>
+        <option key='all' value='all'>All</option>
+        {uniqueHornValues.sort((a, b) => a - b)
+          .map(element => <option key={element} value={element}>{`${element} ${element === 1 ? 'horn' : 'horns'}`}</option>)}
+      </>
+    );
+  }
+
   render() {
     return (
       <Container>
         <Form>
           <Form.Label>Filter by number of horns</Form.Label>
           <Form.Select onChange={this.props.filterBeasts}>
-            <option value='All'>All</option>
-            <option value='1'>1 horn</option>
-            <option value='2'>2 horns</option>
-            <option value='3'>3 horns</option>
-            <option value='100'>100 horns</option>
+            {this.getUniqueOptions()}
           </Form.Select>
         </Form>
       </Container>
